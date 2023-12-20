326ce4e412eb8e065d9e2919a5a7ac3c3ba7c2ce
smackeem <smackeem@outlook.com>
2023-12-20 02:00:24 -0500

    Filter some data to have it look more user friendly.

--- frontend/src/pages/Flights/index.jsx
+++ frontend/src/pages/Flights/index.jsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Flights = ({ allFlights }) => {
-
+    const [airline, setAirline] = useState('')
+    const fetchData = async () => {
+        const response = await fetch('../../../public/airlines.json');
+        const data = await response.json()
+        setAirline(data)
+      }
+    
+      useEffect(() => {
+        fetchData()
+      }, [])
+      let bool;
     return (
         <div className="d-flex m-3 flex-column">
             <h1>Flights</h1>
@@ -9,16 +19,15 @@ const Flights = ({ allFlights }) => {
                 {allFlights.map((flight, idx) => (
                     <div key={idx} className="d-flex p-5 m-2 align-items-start flex-column rounded border ">
                         <p>Price: {flight.price}</p>
-                        <p>Validating Airline Codes: {flight.validatingAirlineCodes.join(', ')}</p>
-
+                        <p> {airline[flight.validatingAirlineCodes.join(', ')]}</p>
+                            { bool = flight.segments.length > 1 ? true : false}
                         {flight.segments.map((segment, sIdx) => (
-                            <div key={sIdx} className="d-flex flex-row card-group m-2 align-items-start rounded border ">
+                            <div key={sIdx} className="d-flex card-group m-2 align-items-start rounded border ">
                                 <section className="card">
-                                <p>Departure Date and Time: {segment.departureDateTime}</p>
+                                <p>{new Date(segment.departureDateTime).toLocaleString()}</p>
                                 <p>Duration: {segment.duration}</p>
-                                <p className="text-muted">{segment.isNonStop ? 'Non-Stop' : 'Connecting'}</p>
-                                <p>Origin Location: {segment.originLocation.cityCode}, {segment.originLocation.countryCode}</p>
-                                <p>Destination Location: {segment.destinationLocation.cityCode}, {segment.destinationLocation.countryCode}</p>
+                                <p className="text-muted">{bool ? 'Non-Stop' : 'Connecting'}</p>
+                                <p>{segment.originLocation.cityCode} - {segment.destinationLocation.cityCode}</p>
                                 </section>
                                 </div>
                         ))}
